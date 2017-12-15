3055cb25ac6c76606ee7b8681b8674b82eaf3a5b
Jburt4 <jburt44@gmail.com>
2017-12-14 22:55:39 -0500

    Runner Class

---
+++ RunYahtzee.py
@@ -0,0 +1,26 @@
+from DiceClass import Dice
+from YahtzeeClass import Yahtzoom
+
+die1 =Dice()
+die1.roll()
+print(die1.value)
+die2 =Dice()
+die2.roll()
+print(die2.value)
+die3 =Dice()
+die3.roll()
+print(die3.value)
+die4 =Dice()
+die4.roll()
+print(die4.value)
+die5 =Dice()
+die5.roll()
+print(die5.value)
+
+#Roll Again Function(Run twice)
+
+#Picking a cateogory Function
+
+#Score Function
+
+#Total Sum Function
--- YahtzeeClass.py
+++ YahtzeeClass.py
@@ -4,44 +4,21 @@ class Yahtzoom:
     def __init__(self):
     	name = 0
 
-    def reroll(self):
-        print("Enter the number of each die you would like to reroll separately. Press 0 to continue.")
-        
-            
+	def reroll(self):
+		print("Enter the number of each die you would like to reroll separately. Press 0 to continue.")
 
+	def yahtzoomprint(self):
 
+	print("▓██   ██▓ ▄▄▄       ██░ ██ ▄▄▄█████▓▒███████▒ ▒█████   ▒█████   ███▄ ▄███▓")
+	print(" ▒██  ██▒▒████▄    ▓██░ ██▒▓  ██▒ ▓▒▒ ▒ ▒ ▄▀░▒██▒  ██▒▒██▒  ██▒▓██▒▀█▀ ██▒")
+	print("  ▒██ ██░▒██  ▀█▄  ▒██▀▀██░▒ ▓██░ ▒░░ ▒ ▄▀▒░ ▒██░  ██▒▒██░  ██▒▓██    ▓██░")
+	print("  ░ ▐██▓░░██▄▄▄▄██ ░▓█ ░██ ░ ▓██▓ ░   ▄▀▒   ░▒██   ██░▒██   ██░▒██    ▒██ ")
+	print("  ░ ██▒▓░ ▓█   ▓██▒░▓█▒░██▓  ▒██▒ ░ ▒███████▒░ ████▓▒░░ ████▓▒░▒██▒   ░██▒")
+	print("   ██▒▒▒  ▒▒   ▓▒█░ ▒ ░░▒░▒  ▒ ░░   ░▒▒ ▓░▒░▒░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒░   ░  ░")
+	print(" ▓██ ░▒░   ▒   ▒▒ ░ ▒ ░▒░ ░    ░    ░░▒ ▒ ░ ▒  ░ ▒ ▒░   ░ ▒ ▒░ ░  ░      ░")
+	print(" ▒ ▒ ░░    ░   ▒    ░  ░░ ░  ░      ░ ░ ░ ░ ░░ ░ ░ ▒  ░ ░ ░ ▒  ░      ░   ")
+	print(" ░ ░           ░  ░ ░  ░  ░           ░ ░        ░ ░      ░ ░         ░   ")
+	print(" ░ ░                                ░                                     ")
 
-
-
-    def Yahtzoom(self):
-
-        print("▓██   ██▓ ▄▄▄       ██░ ██ ▄▄▄█████▓▒███████▒ ▒█████   ▒█████   ███▄ ▄███▓")
-        print(" ▒██  ██▒▒████▄    ▓██░ ██▒▓  ██▒ ▓▒▒ ▒ ▒ ▄▀░▒██▒  ██▒▒██▒  ██▒▓██▒▀█▀ ██▒")
-        print("  ▒██ ██░▒██  ▀█▄  ▒██▀▀██░▒ ▓██░ ▒░░ ▒ ▄▀▒░ ▒██░  ██▒▒██░  ██▒▓██    ▓██░")
-        print("  ░ ▐██▓░░██▄▄▄▄██ ░▓█ ░██ ░ ▓██▓ ░   ▄▀▒   ░▒██   ██░▒██   ██░▒██    ▒██ ")
-        print("  ░ ██▒▓░ ▓█   ▓██▒░▓█▒░██▓  ▒██▒ ░ ▒███████▒░ ████▓▒░░ ████▓▒░▒██▒   ░██▒")
-        print("   ██▒▒▒  ▒▒   ▓▒█░ ▒ ░░▒░▒  ▒ ░░   ░▒▒ ▓░▒░▒░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒░   ░  ░")
-        print(" ▓██ ░▒░   ▒   ▒▒ ░ ▒ ░▒░ ░    ░    ░░▒ ▒ ░ ▒  ░ ▒ ▒░   ░ ▒ ▒░ ░  ░      ░")
-        print(" ▒ ▒ ░░    ░   ▒    ░  ░░ ░  ░      ░ ░ ░ ░ ░░ ░ ░ ▒  ░ ░ ░ ▒  ░      ░   ")
-        print(" ░ ░           ░  ░ ░  ░  ░           ░ ░        ░ ░      ░ ░         ░   ")
-        print(" ░ ░                                ░                                     ")
-
-die1 =Dice()
-die1.roll()
-print(die1.value)
-die2 =Dice()
-die2.roll()
-print(die2.value)
-die3 =Dice()
-die3.roll()
-print(die3.value)
-die4 =Dice()
-die4.roll()
-print(die4.value)
-die5 =Dice()
-die5.roll()
-print(die5.value)
-
-rollagain = str(input('Would you like to roll again? Y/N'))
-if rollagain =='y':
-	
+	def score(self):
+		print('Which cateogory would you like your score to go into?')
